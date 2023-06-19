976b855c5a1c4a2cfa862c35872d26d308e8b2df
aminjh <jhaidriamin@gmail.com>
2023-06-19 16:11:43 -0500

    first step access to file page

---
+++ src/test/java/com/cydeo/tests/AMIN/TestBase.java
@@ -0,0 +1,21 @@
+package com.cydeo.tests.AMIN;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUpMethod() {
+
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+}
